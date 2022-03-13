76f3f5ad4ea8fffdf1d06d8039b369407e7e8ad4
PaulaZz <s24253@pjwstk.edu.pl>
2022-03-13 13:56:48 +0100

    aktualizacja test

---
+++ src/com/cwiczenia/Main.java
@@ -0,0 +1,22 @@
+package com.cwiczenia;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //ZAD 2
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Podaj pierwszą liczbę całkowitą ");
+        int a=sc.nextInt();
+
+        System.out.print("Podaj drugą liczbę całkowitą ");
+        int b=sc.nextInt();
+
+        System.out.print("Podaj trzecią liczbę całkowitą ");
+        int c=sc.nextInt();
+
+
+    }
+}
